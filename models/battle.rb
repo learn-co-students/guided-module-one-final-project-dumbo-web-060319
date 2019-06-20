cf0fa3c452636b43cb1245e4769e4b5161c8ce98
Francis Mendez <fmendez884@gmail.com>
2019-06-20 16:09:52 -0400

    modify battle.rb

--- models/battle.rb
+++ models/battle.rb
@@ -132,29 +132,6 @@ class Battle < ActiveRecord::Base
 
 	end 
 
-
-advantage_table = 
-
-{
-
-	"fire":	 %w"0.5	0.5	2	1	2	1	1	1	1	1	0.5	2	1	1	0.5",
-    "water":	 %w"2	0.5	0.5	1	1	1	1	1	1	2	2	1	1	1	0.5",
-    "grass":	 %w"0.5	2	0.5	1	1	1	1	1	0.5	2	2	0.5	0.5	1	0.5",
-    "electric":    	%w"1	2	0.5	0.5	1	1	1	1	2	0	1	1	1	1	0.5",
-    "ice":	   %w"1	0.5	2	1	0.5	1	1	1	2	2	1	1	1	1	2",
-    "psychic":    	%w"1	1	1	1	1	0.5	1	2	1	1	1	1	2	1	1",
-    "normal":	    %w"1	1	1	1	1	1	1	1	1	1	0.5	1	1	0	1",
-    "fighting":    	%w"1	1	1	1	2	0.5	2	1	0.5	1	2	0.5	0.5	0	1",
-    "flying":	    %w"1	1	2	0.5	1	1	1	2	1	1	0.5	2	1	1	1",
-    "ground":	    %w"2	1	0.5	2	1	1	1	1	0	1	2	0.5	2	1	1",
-    "rock":	  %w"2	1	1	1	2	1	1	0.5	2	0.5	1	2	1	1	1",
-    "bug":	   %w"0.5	1	2	1	1	2	1	0.5	0.5	1	1	1	2	1	1",
-    "poison":	    %w"1	1	2	1	1	1	1	1	1	0.5	0.5	2	0.5	0.5	1",
-    "ghost":	 %w"1	1	1	1	1	0	0	1	1	1	1	1	1	2	1",
-    "dragon":	    %w" 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1"
-
-}
-
 $types = %w"Fire Water Grass Electic	Ice	Psychic	Normal Fighting	Flying Ground Rock Bug Poison Ghost	Dragon"
 
 # {
@@ -192,7 +169,32 @@ $types = %w"Fire Water Grass Electic	Ice	Psychic	Normal Fighting	Flying Ground R
 	# 	end
 	# 	new_hash
 	# 	binding.pry
-  	# end
-advantage_frame = Daru::DataFrame.new(advantage_table, index: advantage_table.keys)
+	  # end
+
+	def self.advantage_frame
+		advantage_table = 
+
+	{
+
+	"fire":	 %w"0.5	0.5	2	1	2	1	1	1	1	1	0.5	2	1	1	0.5",
+    "water":	 %w"2	0.5	0.5	1	1	1	1	1	1	2	2	1	1	1	0.5",
+    "grass":	 %w"0.5	2	0.5	1	1	1	1	1	0.5	2	2	0.5	0.5	1	0.5",
+    "electric":    	%w"1	2	0.5	0.5	1	1	1	1	2	0	1	1	1	1	0.5",
+    "ice":	   %w"1	0.5	2	1	0.5	1	1	1	2	2	1	1	1	1	2",
+    "psychic":    	%w"1	1	1	1	1	0.5	1	2	1	1	1	1	2	1	1",
+    "normal":	    %w"1	1	1	1	1	1	1	1	1	1	0.5	1	1	0	1",
+    "fighting":    	%w"1	1	1	1	2	0.5	2	1	0.5	1	2	0.5	0.5	0	1",
+    "flying":	    %w"1	1	2	0.5	1	1	1	2	1	1	0.5	2	1	1	1",
+    "ground":	    %w"2	1	0.5	2	1	1	1	1	0	1	2	0.5	2	1	1",
+    "rock":	  %w"2	1	1	1	2	1	1	0.5	2	0.5	1	2	1	1	1",
+    "bug":	   %w"0.5	1	2	1	1	2	1	0.5	0.5	1	1	1	2	1	1",
+    "poison":	    %w"1	1	2	1	1	1	1	1	1	0.5	0.5	2	0.5	0.5	1",
+    "ghost":	 %w"1	1	1	1	1	0	0	1	1	1	1	1	1	2	1",
+    "dragon":	    %w" 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1	 1"
+
+	}
+
+		Daru::DataFrame.new(advantage_table, index: advantage_table.keys)
+	end 
 
 end
